c5d97e061b8c1a7e07732f140cf0c31a25bbf019
Andrew Marshall <planetmarshalluk@gmail.com>
2024-02-04 20:58:47 +0000

    add flake8 to CI

--- lldb_qt_formatters/__init__.py
+++ lldb_qt_formatters/__init__.py
@@ -1,11 +1,12 @@
+# We need to import the formatters like this - no other method will work
 from .qstring import *
 from .qmap import *
 
 
-def __lldb_init_module(debugger, dict):
+def __lldb_init_module(debugger, internal_dict):
     init_commands = [
         'type summary add --python-function lldb_qt_formatters.qstring.format_summary "QString"',
-        'type summary add --summary-string "\{${var.key}: ${var.value}\}" -x "QMapNode<.+>$"',
+        'type summary add --summary-string "\{${var.key}: ${var.value}\}" -x "QMapNode<.+>$"',  # noqa: W605
         'type synthetic add --python-class lldb_qt_formatters.qmap.SyntheticChildrenProvider -x "QMap<.+>$"',
         'type summary add --expand --python-function lldb_qt_formatters.qmap.format_summary -x "QMap<.+>$"'
     ]
